ddc0b3f0de0f45c9da933e918734e0366ecc5461
unknown <scundanomine21@gmail.com>
2022-11-01 22:05:18 +0530

    wow

---
+++ src/mycomponents/TodosList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TodosList extends Component {
+  render() {
+    let {item} =this.props
+    return (
+      <div>
+            
+      </div>
+    )
+  }
+}
+
+export default TodosList
